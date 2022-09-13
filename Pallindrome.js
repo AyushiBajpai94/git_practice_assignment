db4986242f40125a8a6970245cc9a535adb771a6
Ayushi Bajpai <ayushi.bajpai16@gmail.com>
2022-09-13 22:22:50 +0530

    added Pallindrome Logic

---
+++ Pallindrome.js
@@ -0,0 +1,14 @@
+
+let str="MAAM";
+let bag="";
+for(let i=str.length-1;i>=0;i++)
+{
+    bag=bag+str[i]
+   }
+if(bag==str)
+{
+console.log("String is Palindrome");
+}
+else{
+console.log("string is not Palindrome");
+}
